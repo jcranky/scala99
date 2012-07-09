71708f235a852a944b7074f172ab32c3beb21e3f
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-07-09 13:42:34 -0300

    skeleton for the problem number 7

---
+++ src/main/scala/com/jcranky/scala99/problem-07.scala
@@ -0,0 +1,14 @@
+package com.jcranky.scala99
+
+/**
+ * Comments published at: ??
+ * 
+ * P07 (**) Flatten a nested list structure.
+ *   Example:
+ *
+ *   scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+ *   res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+ */
+object Problem07 {
+  def bla = false
+}
---
+++ src/test/scala/com/jcranky/scala99/problem-07-spec.scala
@@ -0,0 +1,11 @@
+package com.jcranky.scala99
+
+import org.specs2.mutable.Specification
+
+class Problem07Spec extends Specification {
+  "testing ..." should {
+    "bla" in {
+      pending
+    }
+  }
+}
