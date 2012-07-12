04095f57d814f6cb7736fa6f88de0955295014f3
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-07-12 15:36:29 -0300

    added Phil Gold's solutions

--- src/main/scala/com/jcranky/scala99/problem-08.scala
+++ src/main/scala/com/jcranky/scala99/problem-08.scala
@@ -24,4 +24,28 @@ object Problem08 {
       }
     ).toList
   }
+  
+  // Phil Gold's solutions:
+  
+  // Standard recursive.
+  def compressRecursive[A](ls: List[A]): List[A] = ls match {
+    case Nil       => Nil
+    case h :: tail => h :: compressRecursive(tail.dropWhile(_ == h))
+  }
+
+  // Tail recursive.
+  def compressTailRecursive[A](ls: List[A]): List[A] = {
+    def compressR(result: List[A], curList: List[A]): List[A] = curList match {
+      case h :: tail => compressR(h :: result, tail.dropWhile(_ == h))
+      case Nil       => result.reverse
+    }
+    compressR(Nil, ls)
+  }
+
+  // Functional.
+  def compressFunctional[A](ls: List[A]): List[A] =
+    ls.foldRight(List[A]()) { (h, r) =>
+      if (r.isEmpty || r.head != h) h :: r
+      else r
+    }
 }
