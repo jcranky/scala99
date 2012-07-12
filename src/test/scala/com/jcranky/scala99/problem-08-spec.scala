40391279e5018b5358361bd7893f60f1ad6ca34b
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-07-12 15:36:14 -0300

    added Phil Gold's solutions

--- src/test/scala/com/jcranky/scala99/problem-08-spec.scala
+++ src/test/scala/com/jcranky/scala99/problem-08-spec.scala
@@ -7,8 +7,20 @@ class Problem08Spec extends Specification {
     val original = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
     val compressed = List('a, 'b, 'c, 'a, 'd, 'e)
     
-    "remove all consecutive duplicates" in {
+    "remove all consecutive duplicates with fold and pattern matching" in {
       Problem08.compressFoldAndPatternMatch(original) must_== compressed
+    }
+    
+    "remove all consecutive duplicates with recursive" in {
+      Problem08.compressRecursive(original) must_== compressed
+    }
+    
+    "remove all consecutive duplicates with tail recursive" in {
+      Problem08.compressTailRecursive(original) must_== compressed
+    }
+    
+    "remove all consecutive duplicates with functional approach" in {
+      Problem08.compressFunctional(original) must_== compressed
     }
   }
 }
